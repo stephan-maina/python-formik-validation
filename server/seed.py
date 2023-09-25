b705cbc112d28a12bdc6b5768f91bfb7a3700e8d
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-25 03:10:03 +0300

    completed..

--- server/seed.py
+++ server/seed.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
 
     # Add sample customer data to the database
     customers_data = [
-        {'name': 'John Doe', 'email': 'john@example.com', 'age': 30},
-        {'name': 'Jane Smith', 'email': 'jane@example.com', 'age': 25},
+        {'name': 'Pop Smoke', 'email': 'popsmokedriller092@gmail.com', 'age': 30},
+        {'name': 'Tupac Shakur', 'email': 'Tupacshakur254@gmail.com', 'age': 25},
         # Add more customer data here as needed
     ]
 
@@ -21,14 +21,4 @@ if __name__ == '__main__':
 
     print("Sample customer data added to the database.")
 
-            age= randint(0, 125),
-            name=fake.name()
-        )
-        customers.append(customer)
 
-    db.session.add_all(customers)
-    db.session.commit()        
-
-if __name__ == '__main__':
-    with app.app_context():
-        make_customers()
